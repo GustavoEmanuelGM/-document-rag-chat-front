28e9f6a299f3778edb98f38999f6abac6dcbeb9a
Gustavo Emanuel <gstemanuel2010@gmail.com>
2025-11-14 16:01:23 -0300

    add notification and icon btn

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,6 @@
 import styles from './Header.module.css'
 import { CiSearch } from "react-icons/ci";
-
+import { FiBell } from 'react-icons/fi';
 function Header() {
   return (
     <header className={styles.header}>
@@ -13,7 +13,15 @@ function Header() {
           className={styles.searchInput} />
       </div>
 
-      <div className="right">icons + avatar</div>
+      <div className={styles.userActions}>
+        <button className={styles.iconButton}>
+          <FiBell size={18} />
+        </button>
+        
+        <img src="/Ippo perfil.jpg" 
+        alt="User profile"
+        className={styles.avatar}/>
+      </div>
 
 
     </header>
