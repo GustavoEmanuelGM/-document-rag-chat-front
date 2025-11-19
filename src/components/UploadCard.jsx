7ba8c8c019842950a94a39433e5f9404614feb43
Gustavo Emanuel <gstemanuel2010@gmail.com>
2025-11-19 16:40:29 -0300

    update files color icons

--- src/components/UploadCard.jsx
+++ src/components/UploadCard.jsx
@@ -2,13 +2,13 @@ import CardBase from "../components/Cards/CardBase"
 import Tags from "../pages/Tags"
 import styles from "./UploadCard.module.css";
 
-import { FaFileLines } from "react-icons/fa6"
+import { FaRegFileLines } from "react-icons/fa6";
 
  function UploadCard({ name, tag, size }) {
   return (
     <CardBase>
        <div className={styles.container}>
-        <FaFileLines className={styles.icon} />
+        <FaRegFileLines className={`${styles.icon} ${styles[tag.toLowerCase()]}`} />
         <p className={styles.name}>{name}</p>
         <div className={styles.info}>
           
