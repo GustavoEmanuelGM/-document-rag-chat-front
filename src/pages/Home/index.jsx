de01f80047bc47ce2f2559e1883066224ed64648
Gustavo Emanuel <gstemanuel2010@gmail.com>
2025-11-25 18:56:48 -0300

    add grid and list btn

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,40 +3,73 @@ import UploadCard from "../../components/UploadCard";
 import styles from "./Home.module.css";
 import { FoldersMock } from "../../mocks/foldersMock";
 import FolderCard from "../../components/FolderCard";
-
+import { useState } from "react";
+import { IoGridOutline } from "react-icons/io5";
+import { CiBoxList } from "react-icons/ci";
 
 function Home() {
+  const [viewMode, setViewMode] = useState("grid");
   return (
 
     <div className={styles.home}>
 
-      <h2>Pastas sugeridas</h2>
+      <div>
+        <h3>Pastas sugeridas</h3>
 
-      <div className={styles.grid}>
-        {FoldersMock.map(folder => (
-          <FolderCard
-            key={folder.id}
-            name={folder.name}
-            items={folder.items}
-          />
-        ))}
+        <div className={styles.grid}>
+          {FoldersMock.map(folder => (
+            <FolderCard
+              key={folder.id}
+              name={folder.name}
+              items={folder.items}
+            />
+          ))}
+        </div>
       </div>
 
-      <h2>Últimos uploads</h2>
 
-      <div className={styles.grid}>
-        {uploadsMock.map(file => (
-          <UploadCard
-            key={file.id}
-            name={file.name}
-            tag={file.tag}
-            size={file.size}
-          />
-        ))}
-      </div>
+      <div className={styles.upfile}>
+        <h3>Últimos uploads</h3>
+        <div>
+          <button onClick={() => setViewMode("grid")}>
+            <IoGridOutline />
+          </button>
+
+          <button onClick={() => setViewMode("list")}>
+            <CiBoxList />
+          </button>
+        </div>
+
+        {viewMode === "grid" && ( 
+        <div className={styles.grid}>
+          {uploadsMock.map(file => (
+            <UploadCard
+              key={file.id}
+              name={file.name}
+              tag={file.tag}
+              size={file.size}
+            />
+          ))}
+        </div>
+        )}
       
+        {viewMode === "list" && (
+          <div className={styles.list}>
+          {uploadsMock.map(file => (
+            <UploadCard
+              key={file.id}
+              name={file.name}
+              tag={file.tag}
+              size={file.size}
+            />
+          ))}
+        </div>
+        )}
+      
+      </div>
 
-     
+
+
     </div>
   );
 }
